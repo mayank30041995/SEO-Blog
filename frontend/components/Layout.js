1771242a1ea37e185ed997146eecc182a094e01c
Mayank Sharma <mayankece155@gmail.com>
2021-05-25 00:03:10 +0530

    fix minor

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -4,7 +4,7 @@ import Header from './Header';
 const Layout = ({ children }) => {
     return (
         <React.Fragment>
-            <Header></Header>
+            <Header/>
             {children}
             <p>footer</p>
         </React.Fragment>
