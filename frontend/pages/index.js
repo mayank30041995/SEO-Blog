d3800aa4dad4f20b7f4e57600d37ccfa31ca4493
Mayank Sharma <mayankece155@gmail.com>
2021-05-24 21:00:54 +0530

    layout page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,12 @@
+import Layout from "../components/Layout";
+
 const Index = () => {
-    console.log('This is index page');
-    return <h2>Index Page</h2>
-}
+  console.log("This is index page");
+  return (
+    <Layout>
+      <p>Index Page</p>
+    </Layout>
+  );
+};
 
 export default Index;
