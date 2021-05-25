69e6b5f9b3f237e78309ff7250f18407cfbdff9e
Mayank Sharma <mayankece155@gmail.com>
2021-05-25 22:02:14 +0530

    signup form

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,13 +1,11 @@
-import Link from "next/link"
 import Layout from "../components/Layout"
+import SignupComponent from "../components/auth/SignupComponent"
 
 const Signup = () => {
     return (
         <Layout>
             <h2>Signup Page</h2>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
+            <SignupComponent />
         </Layout>
     )
 }
