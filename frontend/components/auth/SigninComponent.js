3fd3dfdaaa3e8c6d3857185ed731ea18376d9465
Mayank Sharma <mayankece155@gmail.com>
2021-05-26 21:50:07 +0530

    saving token and user in cookie and localstorage

--- frontend/components/auth/SigninComponent.js
+++ frontend/components/auth/SigninComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { signin } from '../../actions/auth';
+import { signin, authenticate} from '../../actions/auth';
 import Router from 'next/router';
 
 const SigninComponent = () => {
@@ -26,7 +26,9 @@ const SigninComponent = () => {
                 // save user token to cookie
                 // save user info to localstorage
                 // authenticate user
-                Router.push(`/`);
+                authenticate(data, () =>{
+                    Router.push(`/`);
+                })            
             }
         });
 }
