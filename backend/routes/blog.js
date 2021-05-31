1fee0212d9d707c08a059135c2be42fe1f13fb6f
Mayank Sharma <mayankece155@gmail.com>
2021-05-31 23:18:21 +0530

    create blog controller

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { time } = require('../controllers/blog');
+const { create } = require('../controllers/blog');
 const router= express.Router()
 
-router.get('/', time)
+const { requireSignin , adminMiddleware } = require('../controllers/auth')
+
+router.post('/blog', requireSignin, adminMiddleware , create)
+
 
 module.exports = router;
